e4f27461150104baf85f27305dd3e390e1bbdb5c
Anthony <darkfrifri7@gmail.com>
2014-12-05 04:32:29 +0100

    Probleme avec le menu ?

--- conf/Menus.ini.php
+++ conf/Menus.ini.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $menus['Crud conseils']="?module=CRUDConseils";
 $menus['Demande aide']="?module=DemandeAide";
 $menus['Exemples']=array(
@@ -11,9 +10,7 @@ $menus['Exemples']=array(
 	'Ajax'=>"?module=Ajax" 
 	);
 $menus['Test']="?module=TestMembre";
-=======
 $menus["Demander de l'aide"]="?module=DemandeAide";
 $menus["Proposer de l'aide"]="?module=Form_Apport_Aide";
 $menus["Les demandes d'aides"]="?module=CRUDAides";
->>>>>>> 8a17623963c438ef492b6c7544c193103b9d7c11
 ?>
