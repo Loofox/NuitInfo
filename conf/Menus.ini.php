e71a951a5d7220ac71548c19dd324bbd1913c561
Anthony <darkfrifri7@gmail.com>
2014-12-05 05:46:32 +0100

    mess

--- conf/Menus.ini.php
+++ conf/Menus.ini.php
@@ -1,6 +1,8 @@
 <?php
-$menus["Demander de l'aide"]="?module=DemandeAide";
-$menus["Proposer de l'aide"]="?module=Form_Apport_Aide";
+$menus['Formulaires']=array(
+	'Demander de l\'aide'=>"?module=DemandeAide",
+	'Proposer de l\'aide'=>"?module=Form_Apport_Aide"
+	);
 $menus["Les demandes d'aides"]="?module=CRUDAides";
 $menus["Les propositions d'aides"]="?module=CRUDAides";
 $menus["Voter !"]="?module=CRUDFinancement";
