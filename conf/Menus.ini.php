521390144a0960c667127e794446d5e8b3c50932
Anthony <darkfrifri7@gmail.com>
2014-12-05 04:24:52 +0100

    menus

--- conf/Menus.ini.php
+++ conf/Menus.ini.php
@@ -1,12 +1,13 @@
 <?php
 $menus['Crud conseils']="?module=CRUDConseils";
+$menus['Demande aide']="?module=DemandeAide";
 $menus['Exemples']=array(
 	'SimpleTPL'=>"?module=SimpleTemplate",
 	'Formulaire'=>"?module=Formulaire",
 	'C.R.U.D'=>"?module=CRUD",	
 	'Redirect'=>"?module=Redirect",
 	'Download'=>"?module=DownloadFile",
-	'Ajax'=>"?module=Ajax"
+	'Ajax'=>"?module=Ajax" 
 	);
 $menus['Test']="?module=TestMembre";
 ?>
